608cad791290d659d7f805a9f1596974ed6c638c
kingJ <jamesdongxin@gmail.com>
2017-10-01 02:09:40 -0400

    Add auto generated controller files

---
+++ src/main/java/com/jdong/passerby/springboot/controller/SysUserRoleController.java
@@ -0,0 +1,20 @@
+package com.jdong.passerby.springboot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author jdong
+ * @since 2017-09-30
+ */
+@Controller
+@RequestMapping("/springboot/sysUserRole")
+public class SysUserRoleController {
+	
+}
