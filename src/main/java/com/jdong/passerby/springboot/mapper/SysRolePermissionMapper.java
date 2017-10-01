45c18017b0cc44d703f70b161ac24b07de189edb
kingJ <jamesdongxin@gmail.com>
2017-10-01 02:08:52 -0400

    Add auto generated mapper files

---
+++ src/main/java/com/jdong/passerby/springboot/mapper/SysRolePermissionMapper.java
@@ -0,0 +1,16 @@
+package com.jdong.passerby.springboot.mapper;
+
+import com.jdong.passerby.springboot.entity.SysRolePermission;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author jdong
+ * @since 2017-09-30
+ */
+public interface SysRolePermissionMapper extends BaseMapper<SysRolePermission> {
+
+}
---
+++ src/main/java/com/jdong/passerby/springboot/mapper/SysUserMapper.java
@@ -0,0 +1,16 @@
+package com.jdong.passerby.springboot.mapper;
+
+import com.jdong.passerby.springboot.entity.SysUser;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author jdong
+ * @since 2017-09-30
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
